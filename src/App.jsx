51cdaef6cf6ada44570fce273acb1d865f8ce6a0
Voo Tzuen Hau <vince_voo@yahoo.com>
2023-08-27 00:17:52 +0800

    updated API

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,69 @@ import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import ParticlesBg from "particles-bg";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [userInput, setNewInput] = useState("");
+  const MODEL_ID = "face-detection";
+
+  const returnClarifaiReqOpt = (imageUrl) => {
+    const PAT = "a2cdea03e55d422a8e1f27f99d40641f";
+    const USER_ID = "kal_serio";
+    const APP_ID = "FaceRecognito";
+
+    const IMAGE_URL = imageUrl;
+
+    const raw = JSON.stringify({
+      user_app_id: {
+        user_id: USER_ID,
+        app_id: APP_ID,
+      },
+      inputs: [
+        {
+          data: {
+            image: {
+              url: IMAGE_URL,
+            },
+          },
+        },
+      ],
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Key " + PAT,
+      },
+      body: raw,
+    };
+
+    return requestOptions;
+  };
+
+  const onInputChange = (e) => {
+    e.preventDefault();
+    setNewInput(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const onButtonSubmit = () => {
+    console.log("clicked");
+  };
+
+  useEffect(() => {
+    if (userInput) {
+      fetch(
+        "https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs",
+        returnClarifaiReqOpt(userInput)
+      )
+        .then((response) => response.json())
+        .then((result) => console.log(result))
+        .catch((error) => console.log("error", error));
+    }
+  }, [userInput]);
+
   return (
     <>
       <div className="App">
@@ -13,7 +74,10 @@ function App() {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={onInputChange}
+          onButtonSubmit={onButtonSubmit}
+        />
         {/* <FaceRecognition /> */}
       </div>
     </>
--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,10 +1,17 @@
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div className="m-2 my-4 py-4 rounded-lg shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)] bg-gradient-to-r from-gray-700 via-gray-900 to-black">
       <p className="p-2">Face detection App</p>
       <div className="p-2 ">
-        <input type="text" className="w-4/5 p-1 rounded my-2" />
-        <button className="relative inline-flex items-center justify-center p-1 px-4 m-1 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500 active:scale-95">
+        <input
+          type="text"
+          className="w-4/5 p-1 rounded my-2"
+          onChange={onInputChange}
+        />
+        <button
+          onClick={onButtonSubmit}
+          className="relative inline-flex items-center justify-center p-1 px-4 m-1 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500 active:scale-95"
+        >
           <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
           <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-3 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
           <span className="relative text-white">Button Text</span>
